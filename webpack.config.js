ad579ab7047f48d58e95734d709aa36f75801b59
LeuAlmeida <leo@webid.net.br>
2019-11-27 15:08:12 -0300

    Including css rule

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,12 @@ module.exports = {
         use: {
           loader: 'babel-loader',
         }
+      }, {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+        ]
       }
     ]
   }
