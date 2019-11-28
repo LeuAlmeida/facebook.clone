94f6a2bce5b6ed52c55b079f85c05f94d8ee12ed
LeuAlmeida <leo@webid.net.br>
2019-11-28 14:16:11 -0300

    Improving App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Header from './components/Header';
+
+import './App.css';
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <Header />
+  )
 }
 
 export default App;
