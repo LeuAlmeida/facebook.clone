8163b7d051a31047df7967e900a522c6a32f2194
LeuAlmeida <leo@webid.net.br>
2019-11-28 17:31:42 -0300

    Import Chat content

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Container from './components/Container/Container';
 import LeftNav from './components/LeftNav/LeftNav';
+import Chat from './components/Chat/Chat';
 
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
 
         <LeftNav />
         <Main />
+        <Chat />
 
       </Container>
     </>
