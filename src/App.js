f359926f96fbd6870808aae612a32adaf5b14850
LeuAlmeida <leo@webid.net.br>
2019-11-28 16:34:57 -0300

    Improving App main content

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import Container from './components/Container/Container';
 
 import './App.css';
 
@@ -9,9 +10,9 @@ function App() {
   return (
     <>
       <Header />
-      {/* <Container className="container"> */}
+      <Container>
       <Main />
-      {/* </Container> */}
+      </Container>
     </>
   );
 }
