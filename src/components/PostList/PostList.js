b224d364f1a87f8f79e27b24350daaab976d6d47
LeuAlmeida <leo@webid.net.br>
2019-12-03 20:37:45 -0300

    Rename PostList

---
+++ src/components/PostList/PostList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './PostList.css';
+
+function PostList() {
+  return (
+    <div className="postFeedCard">
+      <div className="postFeedCardHeader">
+        
+      </div>
+    </div>
+  )
+}
+
+export default PostList;
