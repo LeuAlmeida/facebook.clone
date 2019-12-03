5d095bcde349df8f045669b810789eb70ee5e692
LeuAlmeida <leo@webid.net.br>
2019-12-03 20:39:36 -0300

    Incrementing PostList into a class component

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import './PostList.css';
+import "./PostList.css";
 
-function PostList() {
-  return (
-    <div className="postFeedCard">
-      <div className="postFeedCardHeader">
-        
+class PostList extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        author: {
+          name: "Julio Alcantara",
+          avatar: "http://url-da-imagem.com/imagem.jpg"
+        },
+        date: "04 Jun 2019",
+        content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: "Diego Fernandes",
+              avatar: "http://url-da-imagem.com/imagem.jpg"
+            },
+            content: "Conteúdo do comentário"
+          }
+        ]
+      },
+      {
+        id: 2
+        // Restante dos dados de um novo post
+      }
+    ]
+  };
+  render() {
+    return (
+      <div className="postFeedCard">
+        <div className="postFeedCardHeader"></div>
       </div>
-    </div>
-  )
+    );
+  }
 }
 
 export default PostList;
