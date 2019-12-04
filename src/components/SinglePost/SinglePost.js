8613903d820284fc0326258af7c270cb6f203fa3
LeuAlmeida <leo@webid.net.br>
2019-12-04 12:47:23 -0300

    Update singlepost component with actions

--- src/components/SinglePost/SinglePost.js
+++ src/components/SinglePost/SinglePost.js
@@ -41,6 +41,21 @@ function SinglePost() {
             1 compartilhamento
           </div>
         </div>
+
+        <div className="postFeedCTA">
+          <div className="CTALike">
+          <i className="far fa-thumbs-up"></i>
+            Like
+          </div>
+          <div className="CTAComment">
+          <i className="far fa-comment-alt"></i>
+            Comment
+          </div>
+          <div className="CTAShare">
+          <i className="fas fa-share"></i>
+            Share
+          </div>
+        </div>
       </div>
   )
 }
