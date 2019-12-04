6bd0197d97aec21b64ee4518aa8ac1645eaf6cd1
LeuAlmeida <leo@webid.net.br>
2019-12-04 13:55:33 -0300

    Inserting data content

--- src/components/SinglePost/SinglePost.js
+++ src/components/SinglePost/SinglePost.js
@@ -1,56 +1,85 @@
-import React from 'react';
+import React from "react";
 
-import './SinglePost.css';
+import "./SinglePost.css";
 
-import PostActions from '../PostActions/PostActions';
-import WriteComment from '../WriteComment/WriteComment';
-import SingleComment from '../SingleComment/SingleComment';
+import PostActions from "../PostActions/PostActions";
+import WriteComment from "../WriteComment/WriteComment";
+import SingleComment from "../SingleComment/SingleComment";
 
 import imageLorem from "../../assets/img/personas/cat.jpg";
-import love from '../../assets/img/reactons/love.png';
-import like from '../../assets/img/reactons/like.png';
-import laugh from '../../assets/img/reactons/laugh.png';
-import sad from '../../assets/img/reactons/sad.png';
-import rage from '../../assets/img/reactons/rage.png';
+import love from "../../assets/img/reactons/love.png";
+import like from "../../assets/img/reactons/like.png";
+import laugh from "../../assets/img/reactons/laugh.png";
+import sad from "../../assets/img/reactons/sad.png";
+import rage from "../../assets/img/reactons/rage.png";
 
-function SinglePost() {
+function PostHeader({ author, date }) {
   return (
-    <div className="postFeedCard">
-        <div className="postFeedCardHeader">
-          <div className="section1header">
-            <div className="postListPic">
-              <img src={imageLorem} />
-            </div>
-            <div className="postListInfos">
-              <span className="postListTitle">Léu Almeida</span>
-              <span className="postListDate">7h</span>
-            </div>
-          </div>
-
-          <div className="optionsHeader"><i className="fas fa-ellipsis-h"></i></div>
+    <div className="postFeedCardHeader">
+      <div className="section1header">
+        <div className="postListPic">
+          <img src={author.avatar} />
         </div>
-
-        <div className="postFeedCardContent">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero enim doloremque quidem odit earum velit sunt maiores ipsam id commodi quibusdam natus dicta, sint assumenda reiciendis obcaecati omnis nam corrupti?
+        <div className="postListInfos">
+          <span className="postListTitle">{author.name}</span>
+          <span className="postListDate">{date}</span>
         </div>
-
-        <div className="postFeedReactons">
-          <div className="section1Reactons">
-            <img src={love} />
-            <img src={laugh} />
-            <span className="reactonCounter">8</span>
-          </div>
-          <div className="section2Reactons">
-            1 compartilhamento
-          </div>
-        </div>
-
-        <PostActions />
-        <WriteComment />
-        
-        <SingleComment />
       </div>
-  )
+
+      <div className="optionsHeader">
+        <i className="fas fa-ellipsis-h"></i>
+      </div>
+    </div>
+  );
 }
 
-export default SinglePost;
+function PostComments({ comments }) {
+  return (
+    <>
+      {comments.map(comment => (
+        <div key={comment.id} className="singleCommentContainer">
+          <img src={comment.author.avatar} />
+          <div className="uniqueComment">
+            <div className="theComment">
+              <a href="#">{comment.author.name}</a> {comment.content}
+            </div>
+            <div className="commentActions">
+              <a href="#">Curtir</a>
+              <a href="#">Responder</a>
+              <span>1h</span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
+
+function PostItem({ author, date, content, comments }) {
+  return (
+    <div className="postFeedCard">
+
+      <PostHeader author={author} date={date} />
+
+      <div className="postFeedCardContent">
+      {content}
+      </div>
+
+      <div className="postFeedReactons">
+        <div className="section1Reactons">
+          <img src={love} />
+          <img src={laugh} />
+          <span className="reactonCounter">8</span>
+        </div>
+        <div className="section2Reactons">1 compartilhamento</div>
+      </div>
+
+      <PostActions />
+      <WriteComment />
+
+      <PostComments comments={comments} />
+    </div>
+  );
+}
+
+export default PostItem;
