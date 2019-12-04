ef02fef6b0046c576fa2c3932c54f02f8347c910
LeuAlmeida <leo@webid.net.br>
2019-12-04 13:35:39 -0300

    Improving SingleComment component

--- src/components/SingleComment/SingleComment.js
+++ src/components/SingleComment/SingleComment.js
@@ -2,11 +2,23 @@ import React from 'react';
 
 import './SingleComment.css';
 
+import image from '../../assets/img/personas/cat.jpg';
+
 function SingleComment() {
   return (
-    <>
-
-    </>
+    <div className="singleCommentContainer">
+      <img src={image} />
+      <div className="uniqueComment">
+        <div className="theComment">
+          <a href="#">Léu Almeida</a> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis dolorum, beatae alias eaque iusto excepturi exercitationem incidunt quam autem? Quas harum quidem ullam error perspiciatis beatae soluta molestias! Tenetur, sint!
+        </div>
+        <div className="commentActions">
+          <a href="#">Curtir</a>
+          <a href="#">Responder</a>
+          <span>1h</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
